d9cb73fdda16d224f5c37e4a6507ec9c25f25561
sultangazin <48042441+sultangazin@users.noreply.github.com>
2023-10-10 16:45:38 -0700

    Chapter 6

--- epi_judge_cpp/convert_base.cc
+++ epi_judge_cpp/convert_base.cc
@@ -4,8 +4,39 @@
 using std::string;
 
 string ConvertBase(const string& num_as_string, int b1, int b2) {
-  // TODO - you fill in here.
-  return "";
+  // check if negative
+  bool is_negative = false; 
+  if (num_as_string.front() == '-') {
+    is_negative = true; 
+  }
+  // convert from b1 to int
+  int num = 0;
+  for (auto digit : num_as_string) {
+    if (digit == '-') {
+      continue; 
+    }
+    num *= b1;
+    if (digit >= '0' && digit <= '9') {
+      num += digit - '0';
+    }
+    else if (digit >= 'A' && digit <= 'F') {
+      num += digit - 'A' + 10;
+    }
+  }
+  
+  // convert from int to b2
+  string s;
+  while (num != 0) {
+    int digit = num % b2;
+    if (digit >= 0 && digit <= 9) {
+      s.push_back(digit + '0');
+    }
+    if (digit >= 10 && digit <= 16) {
+      s.push_back(digit - 10 + 'A');
+    }
+    num /= b2;
+  }
+  return s;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -1,16 +1,44 @@
 #include <string>
+#include <algorithm>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 using std::string;
 
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "0";
+  bool is_negative = false;
+  if (x < 0) {
+    is_negative = true;
+    x = -x;
+  }
+  if (x == 0) {
+    return "0";
+  }
+  string result = "";
+  while (x != 0) {
+    result.push_back((x % 10) + '0');
+    x /= 10;
+  }
+  if (is_negative) {
+    result.push_back('-');
+  }
+  std::reverse(result.begin(), result.end());
+  return result;
 }
 int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+  int result = 0;
+  bool is_negative = false; 
+  for (auto num : s) {
+    if (num == '-') {
+      is_negative = true; 
+    }
+    result *= 10;
+    result += num - '0';
+  }
+  if (is_negative) {
+    result = -result;
+  }
+  return result;
 }
 void Wrapper(int x, const string& s) {
   if (stoi(IntToString(x)) != x) {
